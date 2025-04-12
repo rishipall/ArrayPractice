f7fdab8048df81365e72bb3587213868719cbb6d
alonecreations <108176909+alonecreations@users.noreply.github.com>
2025-04-12 12:48:46 +0530

    target

--- script.js
+++ script.js
@@ -57,15 +57,33 @@
 
 
 // reverse the array using two pointer algorithm 
-let arr = [1,2,3,4,5,6,7,8,9,10]
-let i = 0
-let j = arr.length-1
-while(i<j){
-    let temp = arr[i]
-    arr[i] = arr[j];
-    arr[j] = temp;
-    i++
-    j--
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let i = 0
+// let j = arr.length-1
+// while(i<j){
+//     let temp = arr[i]
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     i++
+//     j--
+// }
+// console.log(arr);
+// console.log(String.fromCharCode(30));
+
+// wap to get the indexs of element which are eqal to target element 
+
+let arr = [3,2,4]
+let target = 5
+let index1 = -1
+let index2 = -1
+
+for(let i = 0;i<arr.length;i++){
+    if(arr[i]+arr[i+1] == target){
+
+         index1 = arr[i]
+         index2 = arr[i+1]
+         break;
+    }
 }
-console.log(arr);
-console.log(String.fromCharCode(30));
+
+console.log(arr.indexOf(index1),arr.indexOf(index2));

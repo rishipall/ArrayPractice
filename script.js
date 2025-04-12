584688e6c4977a168df5844bdca149cc013643b1
alonecreations <108176909+alonecreations@users.noreply.github.com>
2025-04-12 12:39:03 +0530

    second

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 // find the intersection of two array 
 
-let arr1 = [6,7,3,1]
-let arr2 = [1,2,3,4,5]
+// let arr1 = [6,7,3,1]
+// let arr2 = [1,2,3,4,5]
 
 
-for(let i = 0;i<arr1.length;i++){
-    for(let j = 0;j<arr2.length;j++){
-            if(arr1[i] ==arr2[j]){
-                console.log(arr1[i]);
-            }
-    }   
+// for(let i = 0;i<arr1.length;i++){
+//     for(let j = 0;j<arr2.length;j++){
+//             if(arr1[i] ==arr2[j]){
+//                 console.log(arr1[i]);
+//             }
+//     }   
+// }
+
+/// wap to print the unique number in an array  
+
+let arr = [1,2,3,1,2,3,4,5,0]
+let unique = [];
+
+for(let i = 0;i<arr.length;i++){
+    let count = 0;
+    for(let j =0;j<arr.length;j++){
+        if(arr[i] == arr[j]){
+            count++;
+        }
+    }
+    if(count == 1){
+        unique.push(arr[i])
+    }
 }
+console.log(unique);
